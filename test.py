098dc123724e7021caeded8c8fd8a53a0c7f9c96
Javier-Fuentes-Senoran <144327430+Javier-Fuentes-Senoran@users.noreply.github.com>
2024-06-06 11:56:07 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait #(html mas lento)
+from selenium.webdriver.support.ui import WebDriverWait #(html slower)
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
